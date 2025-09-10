228c6461f73de83b6bee8e5d1f38bc53138fa5a8
duke.kimm <duke.kimm7@gmail.com>
2025-09-10 14:55:48 +0900

    feat(frontend): add sniff window with participants, host-aligned chat, and staged sequential demo (2s interval); multi-select and Sniff button; route via query param

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import SniffApp from './SniffApp'
 
+const params = new URLSearchParams(window.location.search)
+const isSniff = params.get('sniff') === '1'
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		{isSniff ? <SniffApp /> : <App />}
 	</React.StrictMode>
 )
